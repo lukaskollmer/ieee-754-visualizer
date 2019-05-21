55ae530c6040500c5095a66a796c65e86f291345
Lukas Kollmer <lukas.kollmer@gmail.com>
2019-05-21 18:39:39 +0200

    color the formula, restructure the code

--- main.js
+++ main.js
@@ -59,14 +59,7 @@ const settings = {
 };
 
 
-// TODO: there must be a better way to access a double's internal representation?
-const _double2IEEE = value => {
-    const buffer = new ArrayBuffer(8);
-    const flt64buf = new Float64Array(buffer);
-    const uint8buf = new Uint8Array(buffer);
-    flt64buf[0] = value;
-    return uint8buf;
-}
+// MARK: Utils
 
 
 const removeAllChildren = elem => {
@@ -75,21 +68,8 @@ const removeAllChildren = elem => {
     }
 }
 
-
-
-
-const setSelectedFormat = arg0 => {
-    document.querySelector('.format-selected').classList.remove('format-selected');
-    if (arg0 instanceof MouseEvent) {
-        arg0.target.classList.add('format-selected');
-        currentFormat = Object.keys(FORMAT)[parseInt(arg0.target.getAttribute('fmt_id'))];
-    } else if (typeof arg0 === 'string') {
-        currentFormat = arg0;
-        document.querySelectorAll('.format-option')[_formatId[arg0]].classList.add('format-selected');
-    }
-    settings.setFormat(currentFormat);
-    setupBitsTable();
-    reloadNumber();
+const setTextContent = (id, textContent) => {
+    document.getElementById(id).textContent = `${textContent}`;
 }
 
 
@@ -112,58 +92,32 @@ const sum = input => {
     return retval;
 }
 
-const reloadNumber = event => {
-    const prop = NumberProperties[currentFormat];
-    const numBits = prop.byteCount * 8;
-
-    if (event instanceof MouseEvent && event.target.classList.contains('bit')) {
-        const value = parseInt(event.target.textContent);
-        event.target.textContent = `${1 - value}`;
-        fullBitPattern[numBits - 1 - parseInt(event.target.getAttribute('lk_bit_index'))] = 1 - value;
-        settings.setBitPattern(fullBitPattern);
-    }
-
-
-    const fractionBitPatternOffset = 1 + prop.exponentWidth;
-    const exponentBitPattern = new Uint8Array(rawBitPattern, 1, prop.exponentWidth);
-    const fractionBitPattern = new Uint8Array(rawBitPattern, fractionBitPatternOffset, numBits - 1 - prop.exponentWidth);
-
-    const exponentZeroOffset = (1 << (prop.exponentWidth - 1)) - 1;
-    const exponentSum = sum(exponentBitPattern);
-    const fractionSum = sum(fractionBitPattern);
-    const implicitLeadingFractionBit = exponentBitPattern.includes(1) ? 1 : 0;
-    
-    const sgn = Math.pow(-1, fullBitPattern[0]);
-    const e = exponentSum === 0 ? 1 : [...Array(prop.exponentWidth).keys()].reduce((acc, i) => {
-        return acc + Math.pow(2, i) * exponentBitPattern[exponentBitPattern.length - i - 1];
-    }, 0);
-    const f = [...Array(numBits - 1 - prop.exponentWidth).keys()].reduce((acc, i) => {
-        return acc + Math.pow(2, -(i + 1)) * fractionBitPattern[i];
-    }, implicitLeadingFractionBit);
-    
-    let number = undefined;
-
-    if (exponentSum === 0) {
-        if (fractionSum === 0) {
-            number = sgn === -1 ? '-0.0' : '0.0';
-        }
-    } else if (exponentSum === exponentBitPattern.length) {
-        if (fractionSum === 0) {
-            number = sgn * Infinity;
-        } else {
-            number = sgn * NaN;
-        }
-    }
-
-    if (number === undefined) {
-        number = sgn * Math.pow(2, e - exponentZeroOffset) * f;
-    }
-
-    document.getElementById('number-preview').value = `${number}`;
-    const formulaDiv = document.getElementById('number-formula');
-    formulaDiv.textContent = `(-1)^${fullBitPattern[0]} * 2^(${e} - ${exponentZeroOffset}) * ${f}`;
+const _double2IEEE = value => {
+    const buffer = new ArrayBuffer(8);
+    const flt64buf = new Float64Array(buffer);
+    const uint8buf = new Uint8Array(buffer);
+    flt64buf[0] = value;
+    return uint8buf;
 }
 
+
+
+// MARK: App
+
+
+const setSelectedFormat = arg0 => {
+    document.querySelector('.format-selected').classList.remove('format-selected');
+    if (arg0 instanceof MouseEvent) {
+        arg0.target.classList.add('format-selected');
+        currentFormat = Object.keys(FORMAT)[parseInt(arg0.target.getAttribute('fmt_id'))];
+    } else if (typeof arg0 === 'string') {
+        currentFormat = arg0;
+        document.querySelectorAll('.format-option')[_formatId[arg0]].classList.add('format-selected');
+    }
+    settings.setFormat(currentFormat);
+    setupBitsTable();
+    reloadNumber();
+}
 
 
 const setNumberFromString = string => {
@@ -181,8 +135,6 @@ const setNumberFromString = string => {
     setupBitsTable();
     reloadNumber();
 }
-
-
 
 const setupBitsTable = () => {
     const numBytes = NumberProperties[currentFormat].byteCount;
@@ -206,17 +158,74 @@ const setupBitsTable = () => {
         bitElem.setAttribute('lk_bit_index', numBits - 1 - i);
         byteElem.appendChild(bitElem);
         if (i == 0) {
-            bitElem.classList.add('bit-style-signbit');
+            bitElem.classList.add('signbit');
             bitElem.title += ' (sign bit)';
         } else if (i < NumberProperties[currentFormat].exponentWidth + 1) {
-            bitElem.classList.add('bit-style-exponent');
+            bitElem.classList.add('exponent');
             bitElem.title += ' (exponent)';
         } else {
-            bitElem.classList.add('bit-style-significand');
-            bitElem.title += ' (fraction)';
+            bitElem.classList.add('significand');
+            bitElem.title += ' (significand)';
         }
     }
 }
+
+
+
+const reloadNumber = event => {
+    const prop = NumberProperties[currentFormat];
+    const numBits = prop.byteCount * 8;
+
+    if (event instanceof MouseEvent && event.target.classList.contains('bit')) {
+        const value = parseInt(event.target.textContent);
+        event.target.textContent = `${1 - value}`;
+        fullBitPattern[numBits - 1 - parseInt(event.target.getAttribute('lk_bit_index'))] = 1 - value;
+        settings.setBitPattern(fullBitPattern);
+    }
+
+    const significandBitPatternOffset = 1 + prop.exponentWidth;
+    const exponentBitPattern = new Uint8Array(rawBitPattern, 1, prop.exponentWidth);
+    const significandBitPattern = new Uint8Array(rawBitPattern, significandBitPatternOffset, numBits - 1 - prop.exponentWidth);
+
+    const exponentZeroOffset = (1 << (prop.exponentWidth - 1)) - 1;
+    const exponentSum = sum(exponentBitPattern);
+    const significandSum = sum(significandBitPattern);
+    const implicitLeadingSignificandBit = exponentBitPattern.includes(1) ? 1 : 0;
+    
+    const sgn = Math.pow(-1, fullBitPattern[0]);
+    const e = exponentSum === 0 ? 1 : [...Array(prop.exponentWidth).keys()].reduce((acc, i) => {
+        return acc + Math.pow(2, i) * exponentBitPattern[exponentBitPattern.length - i - 1];
+    }, 0);
+    const f = [...Array(numBits - 1 - prop.exponentWidth).keys()].reduce((acc, i) => {
+        return acc + Math.pow(2, -(i + 1)) * significandBitPattern[i];
+    }, implicitLeadingSignificandBit);
+    
+    let number = undefined;
+
+    if (exponentSum === 0) {
+        if (significandSum === 0) {
+            number = sgn === -1 ? '-0.0' : '0.0';
+        }
+    } else if (exponentSum === exponentBitPattern.length) {
+        if (significandSum === 0) {
+            number = sgn * Infinity;
+        } else {
+            number = sgn * NaN;
+        }
+    }
+
+    if (number === undefined) {
+        number = sgn * Math.pow(2, e - exponentZeroOffset) * f;
+    }
+
+    document.getElementById('number-preview').value = number;
+    setTextContent('formula.sign_bit', fullBitPattern[0]);
+    setTextContent('formula.exp_val', e);
+    setTextContent('formula.exp_offset', exponentZeroOffset);
+    setTextContent('formula.significand', f);
+}
+
+
 
 const main = () => {
     for (const elem of document.getElementsByClassName('format-option')) {
@@ -240,6 +249,5 @@ const main = () => {
         reloadNumber();
     }
 };
-
 
 document.addEventListener('DOMContentLoaded', main);
